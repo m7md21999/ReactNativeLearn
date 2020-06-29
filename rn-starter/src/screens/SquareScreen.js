62362d1566039a3efbda22e67af5119d383bfc74
Mohammed <m7md21999@gmail.com>
2020-06-29 21:05:34 +0300

    did a simple app with a color view element

--- rn-starter/src/screens/SquareScreen.js
+++ rn-starter/src/screens/SquareScreen.js
@@ -1,13 +1,42 @@
 import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
-import SquareDet from "../components/SquareDet";
+import ColorCounter from "../components/ColorCounter";
 
+const COLOR_INCREMENT = 15;
 const SquareScreen = () => {
+  const [red, setRed] = useState(0);
+  const [blue, setBlue] = useState(0);
+  const [green, setGreen] = useState(0);
+
   return (
     <View>
       <Text>SquareScreen Page</Text>
 
-      <SquareDet title="red" />
+      <ColorCounter
+        onIncrease={() => setRed(red + COLOR_INCREMENT)}
+        onDecrease={() => setRed(red - COLOR_INCREMENT)}
+        color="Red"
+      />
+
+      <ColorCounter
+        onIncrease={() => setBlue(blue + COLOR_INCREMENT)}
+        onDecrease={() => setBlue(blue - COLOR_INCREMENT)}
+        color="Blue"
+      />
+
+      <ColorCounter
+        onIncrease={() => setGreen(green + COLOR_INCREMENT)}
+        onDecrease={() => setGreen(green - COLOR_INCREMENT)}
+        color="Green"
+      />
+
+      <View
+        style={{
+          height: 150,
+          width: 150,
+          backgroundColor: `rgb(${red},${blue},${green})`,
+        }}
+      />
     </View>
   );
 };
